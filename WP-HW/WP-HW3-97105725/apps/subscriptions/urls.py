9df46b9ae639fde604d4f11f2184e22d8a65767e
M-H-Boroumandnia <118667895+M-H-Boroumandnia@users.noreply.github.com>
2026-07-23 23:00:29 +0330

    Fix directory mismatching

--- WP-HW/WP-HW3-97105725/apps/subscriptions/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import PlansView, PurchaseView, SubscriptionStatusView
-
-urlpatterns = [
-    path("status/", SubscriptionStatusView.as_view(), name="subscription-status"),
-    path("plans/", PlansView.as_view(), name="subscription-plans"),
-    path("purchase/", PurchaseView.as_view(), name="subscription-purchase"),
-]
